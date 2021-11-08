18c4902f39596dd808711ec82ef3f0ecfbc79250
bartwr <mail@bartroorda.nl>
2021-11-08 23:57:10 +0100

    Navigation: Sort nav menu alphabetically

--- imports/ui/components/Navigation/Navigation.jsx
+++ imports/ui/components/Navigation/Navigation.jsx
@@ -3,17 +3,31 @@ import React, { useState, useEffect } from 'react';
 import './Navigation.css';
 
 export const Navigation = () => {
+  const navItemsToExclude = [
+    'TEMPLATE',
+    'Kennisbank home'
+  ];
   const [folderDocs, setFolderDocs] = useState([]);
+
+  // Sort array on sub key
+  const sortAlphabetically = (elements, key) => {
+    return elements.sort((a, b) => {
+      return a[key].toLowerCase() < b[key].toLowerCase() ? -1 : 1;
+    });
+  }
 
   useEffect(() => {
     Meteor.call('drive.getFolderFiles', '148bWv4FCGEeTBeEgwZCFjT7gn748s3vj', (err, res) => {
-      setFolderDocs(res);
+      setFolderDocs(
+        sortAlphabetically(res, 'name')
+      );
     });
   }, []);
 
   return (
     <div className="Navigation">
       {folderDocs && folderDocs.map(x => {
+        if(navItemsToExclude.indexOf(x.name) > -1) return;
         return <a href="#" key={x.id} onClick={(e) => {
           e.preventDefault();
           FlowRouter.go('/d/' + x.id);
