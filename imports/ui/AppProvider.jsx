3a5572a2a86d57830eeeacd45855a1e4ef74cad5
bartwr <mail@bartroorda.nl>
2021-11-21 21:41:26 +0100

    Hotfix at AppProvider: forgot to include {compose} from redux

--- imports/ui/AppProvider.jsx
+++ imports/ui/AppProvider.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 import appReducer from '../reducers';
 
