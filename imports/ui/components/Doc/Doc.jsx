5b5716877d1f5b94799840db0abfb82549bbc67b
naomiaz <29675763+naomiaz@users.noreply.github.com>
2021-11-22 22:48:15 +0100

    Add Table of Contents, but for now only show on desktop

--- imports/ui/components/Doc/Doc.jsx
+++ imports/ui/components/Doc/Doc.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { saveDocTitle, saveDocContent } from '/imports/reducers/doc'
 import { findDocument } from '/imports/reducers/doc'
 import { marked } from 'marked'
+import { isDesktop } from '/imports/ui/AppUtils'
 
 const homeUnicodeSymbols = ['🏠', '🏡', '🏞️', '🌉', '🌃', '🏙️', '🌆', '🌌', '🎪', '🏕️']
 
@@ -13,14 +14,15 @@ export const Doc = (props) => {
   // State variables
   const [title, setTitle] = useState('')
   const [doc, setDoc] = useState('')
+  const [isToCOpen, setToCMode] = useState(false)
 
   // Function fetchDoc :: fetched document from Google Docs
   const fetchDoc = () => {
     // Get document from store
     const documentFromStore = findDocument(documentsArray, props.documentId)
-    if(documentFromStore) {
-      setTitle(documentFromStore.title);
-      setDoc(documentFromStore.content);
+    if (documentFromStore) {
+      setTitle(documentFromStore.title)
+      setDoc(documentFromStore.content)
     } else {
       // Get random 'loading' title
       const randomLoadingTitle = homeUnicodeSymbols[Math.floor(Math.random() * homeUnicodeSymbols.length)]
@@ -32,10 +34,12 @@ export const Doc = (props) => {
     Meteor.call('docs.getFormattedDoc', props.documentId, (err, docInMarkdown) => {
       setDoc(docInMarkdown)
       // Save doc content in Redux store
-      dispatch(saveDocContent({
-        documentId: props.documentId,
-        content: docInMarkdown
-      }));
+      dispatch(
+        saveDocContent({
+          documentId: props.documentId,
+          content: docInMarkdown
+        })
+      )
       // Save doc content in database
       Meteor.call('docs.updateDocContent', {
         documentId: props.documentId,
@@ -49,10 +53,12 @@ export const Doc = (props) => {
     Meteor.call('docs.getTitle', props.documentId, (err, title) => {
       setTitle(title)
       // Save doc content in Redux store
-      dispatch(saveDocTitle({
-        documentId: props.documentId,
-        title: title
-      }));
+      dispatch(
+        saveDocTitle({
+          documentId: props.documentId,
+          title: title
+        })
+      )
       // Save doc title in database
       Meteor.call('docs.updateDocTitle', {
         documentId: props.documentId,
@@ -69,25 +75,88 @@ export const Doc = (props) => {
   // Strip comments from doc
   const strippedDoc = doc && doc.indexOf('---') > -1 ? doc.split('---')[2] : doc ? doc : ''
 
-  return (
-    <section className='Content text--styled'>
-      <h1>
-        {title}
-        <a
-          href={`https://docs.google.com/document/d/${props.documentId}/edit`}
-          target='_blank'
-          rel='external'
-          title='Bewerk in Google Docs'
-          style={{marginLeft: '1rem', fontSize: '1rem', fontWeight: 'normal'}}>
-          bewerk
-        </a>
-      </h1>
+  // Generate Table of Contents with different levels
+  // https://github.com/markedjs/marked/issues/545#issuecomment-495093214
+  const toc = []
+  var renderer = (function () {
+    var renderer = new marked.Renderer()
+    renderer.heading = function (text, level, raw) {
+      var anchor = this.options.headerPrefix + raw.toLowerCase().replace(/[^\w\\u4e00-\\u9fa5]]+/g, '-')
+      toc.push({ anchor: anchor, level: level, text: text })
+      return `<h2>${text} <span id="${anchor}" /></h2>`
+    }
+    return renderer
+  })()
 
-      <div
-        dangerouslySetInnerHTML={{
-          __html: marked(strippedDoc, { breaks: true })
-        }}
-      />
-    </section>
+  marked.setOptions({ renderer: renderer })
+  function build(coll, k, level, ctx) {
+    if (k >= coll.length || coll[k].level <= level) {
+      return k
+    }
+    var node = coll[k]
+    ctx.push(`<li><a href='#${node.anchor}'>${node.text}</a>`)
+    k++
+    var childCtx = []
+    k = build(coll, k, node.level, childCtx)
+    if (childCtx.length > 0) {
+      ctx.push('<ol>')
+      childCtx.forEach(function (idm) {
+        ctx.push(idm)
+      })
+      ctx.push('</ol>')
+    }
+    ctx.push('</li>')
+    k = build(coll, k, level, ctx)
+    return k
+  }
+  var html = marked(strippedDoc)
+  var ctx = []
+  ctx.push('<ol>')
+  build(toc, 0, 0, ctx)
+  ctx.push('</ol>')
+
+  toggleToc = () => {
+    setToCMode((isToCOpen) => !isToCOpen)
+    document.documentElement.classList.toggle('ToC--toggled')
+  }
+
+  closeToc = () => {
+    setToCMode(false)
+    document.documentElement.classList.remove('ToC--toggled')
+  }
+  return (
+    <>
+      <section className='ToC'>
+        <div className='ToC__inner'>
+          {/* Title shows on desktop, button should show on mobile */}
+          <h2 className='ToC__title'>Inhoud</h2>
+          {/* <button className='ToC__toggle' aria-expanded={isToCOpen} onClick={() => toggleToC()}>
+            Inhoud
+          </button> */}
+          <div aria-hidden={!isToCOpen} dangerouslySetInnerHTML={{ __html: ctx.join('') }} />
+        </div>
+      </section>
+
+      <section className='Content text--styled'>
+        <h1>
+          <span className='Content__title'>{title}</span>
+          <a
+            href={`https://docs.google.com/document/d/${props.documentId}/edit`}
+            target='_blank'
+            rel='external'
+            title='Bewerk in Google Docs'
+            style={{ marginLeft: '1rem', fontSize: '1rem', fontWeight: 'normal' }}
+          >
+            bewerk
+          </a>
+        </h1>
+
+        <div
+          dangerouslySetInnerHTML={{
+            __html: marked(strippedDoc, { breaks: true })
+          }}
+        />
+      </section>
+    </>
   )
 }
