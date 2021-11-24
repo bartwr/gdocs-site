4fa9adcde70eff6df7db29e2b2bc757fcc59a2cf
bartwr <mail@bartroorda.nl>
2021-11-24 20:29:46 +0100

    Redirect to kennisbank.nijverhoekrotterdam.nl if not there already

--- server/main.js
+++ server/main.js
@@ -1,3 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 
 import '/imports/server/methods/index.js';
+
+WebApp.rawConnectHandlers.use(
+  (req, res, next) => {
+
+    /**
+     * Redirect incorrect URL to correct URL (in production)
+     */
+    if (
+      process.env.NODE_ENV != 'development' &&
+      ! req.headers.host.includes('kennisbank.')
+    ) {
+      res.writeHead(
+        301, {
+          'Location': 'https://kennisbank.nijverhoekrotterdam.nl' + req.originalUrl,
+        }
+      );
+      return res.end();
+    }
+
+    /**
+     * Keep going
+     * /!\ DO NOT DELETE /!\
+     */
+    return next();
+  }
+);
